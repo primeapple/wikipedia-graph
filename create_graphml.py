48b54f8fc77c78c0a935877ae80d38dc24b9d756
primeapple <toni.mueller.web@mailbox.org>
2020-06-11 09:20:05 +0200

    refactored method to create graphml file

--- create_graphml.py
+++ create_graphml.py
@@ -2,20 +2,25 @@ import json
 from pygraphml import GraphMLParser
 from pygraphml import Graph
 
-NAME_OF_INPUT_FILE = 'article_with_links_and_categories.json'
-NAME_OF_OUTPUT_FILE = 'graph.graphml'
-
-with open(NAME_OF_INPUT_FILE) as f:
-    data = json.load(f)
+NAME_OF_INPUT_FILE = 'results/wikipages.json'
+NAME_OF_OUTPUT_FILE = 'results/graph3.graphml'
 
 
-class Article:
-    def __init__(self, id, title, url, categories):
-        self.id = id
-        self.title = title
+class Wikipage:
+    def __init__(self, url, title, id=None, mainCategory=None):
         self.url = url
-        self.categories = categories
-        self.links_to = []
+        self.title = title
+        self.id = id
+        self.mainCategory = mainCategory
+        self.linksTo = set()
+
+    def addLink(self, linkedPageId):
+        if (linkedPageId in self.linksTo):
+            # print("Skip link ", linkedPageId, " for page ", self.title)
+            return False
+        else:
+            self.linksTo.add(linkedPageId)
+            return True
     
     def __eq__(self, another):
         return hasattr(another, 'url') and self.url == another.url
@@ -23,49 +28,79 @@ class Article:
     def __hash__(self):
         return hash(self.url)
 
-    def add_link(self, linked_article_id):
-        self.links_to.append(linked_article_id)
+    def __str__(self):
+        return "Page [TITLE=\'" + self.title + "\', URL=" + self.url
 
-graph = Graph()
-graph.directed = False
-nodes = []
-articles = []
-url_to_id = {}
-current_id = 0
+def main():
+    with open(NAME_OF_INPUT_FILE) as f:
+        data = json.load(f)
+    wikipages = createWikipages(data)
+    print(len(wikipages))
+    writeToGraphml(wikipages, NAME_OF_OUTPUT_FILE)
 
-for a in data:
-    url_to_id[a['url']] = current_id
-    if 'title' in a:
-        a_title = a['title']     
-    else: 
-        a_title = "unknown title"
-        # print("Unknown title for", a['url'])
-    if 'categories' in a:
-        a_cat = a['categories']     
-    else: 
-        a_cat = []
-        # print("Unknown categories for", a['url'])
+def createWikipages(jsonData):
+    categories = createCategories(jsonData)
+    pages = []
+    urlToId = {}
+    # creating all the nodes
+    for currentId, currentPage in enumerate(jsonData):
+        urlToId[currentPage['url']] = currentId
+        mainCategory = getMainCategory(currentPage['categories'], categories)
+        page = Wikipage(currentPage['url'], currentPage['title'], id=currentId, mainCategory=mainCategory)
+        pages.append(page)
+    # creating all the edges
+    for currentPage in jsonData:
+        currentPageId = urlToId[currentPage['url']]
+        if 'links' in currentPage:
+            for link in currentPage['links']:
+                if link['url'] in urlToId:
+                    pages[currentPageId].addLink(urlToId[link['url']])
+    return pages
 
-    # article = Article(current_id, a_title, a['url'], a_cat)
-    # articles.append(article)
-    node = graph.add_node(a_title)
-    node['url'] = a['url']
-    node['categories'] = a_cat
-    node['id'] = current_id
-    nodes.append(node)
-    current_id += 1
+# creating all the categories
+def createCategories(jsonData):
+    categories = {}
+    for page in jsonData:
+        for category in page['categories']:
+            catObject = Wikipage(category['url'], category['title'])
+            if catObject in categories:
+                categories[catObject] += 1
+            else:
+                categories[catObject] = 1
+    # categoriesSortedByAppearanceDesc = [key for key, _ in sorted(categories.items(), key=lambda item: item[1], reverse=True)]
+    return categories
 
-for a in data:
-    current_article_id = url_to_id[a['url']]
-    if 'links' in a:
-        for link in a['links']:
-            if link['url'] in url_to_id:
-                graph.add_edge(nodes[current_article_id], nodes[url_to_id[link['url']]])
-                # articles[current_article_id].add_link(url_to_id[link['url']])
-    # else:
-    #     print("Unknown links for", a['url'])
+# get the one category in categoryList, that occurs most inin categories
+def getMainCategory(categoryList, categories):
+    currentMainCat = None
+    appearanceMainCat = 0
+    for c in categoryList:
+        catObject = Wikipage(c['url'], c['title'])
+        if categories[catObject] > appearanceMainCat:
+            currentMainCat = catObject
+            appearanceMainCat = categories[catObject]
+    return currentMainCat
 
 
-# with open(NAME_OF_OUTPUT_FILE, "w+") as f:
-parser = GraphMLParser()
-parser.write(graph, NAME_OF_OUTPUT_FILE)
+def writeToGraphml(pages, fileName):
+    graph = Graph()
+    nodes = []
+    # creating nodes
+    for page in pages:
+        node = graph.add_node(page.id)
+        node['title'] = page.title
+        node['url'] = page.url
+        if not page.mainCategory is None:
+            node['main_category'] = page.mainCategory.title
+        nodes.append(node)
+    # creating edges
+    for page in pages:
+        # if there are edges
+        if page.linksTo:
+            for pageId in page.linksTo:
+                e = graph.add_edge(nodes[page.id], nodes[pageId])
+                e.set_directed(True)
+    parser = GraphMLParser()
+    parser.write(graph, fileName)
+
+main()
